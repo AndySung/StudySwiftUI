518f6ebc8e19aa1e5fb6555c1402059653ba3b7c
andysung <songzongjian0123@163.com>
2024-05-20 12:35:51 +0800

    first commit

---
+++ StudySwiftUI/StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by andy on 2024/5/20.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
